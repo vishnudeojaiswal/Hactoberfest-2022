bb6ef89eafeefdb648b1287674373fd2cf3fa868
bhavna_panjwani <bhavnapanjwani745@gmail.com>
2022-10-03 16:41:35 +0530

    added array and linkedlist implementation of queue

---
+++ C++/queue/linkedListImplementation.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+struct Node
+{int data;
+Node *next;
+Node(int x){
+    data=x;
+    next=NULL;
+}
+    
+};
+struct Queue
+{
+    Node *front,*rear;
+    int size;
+    Queue(){
+        front=NULL;
+        rear=NULL;
+        size=0;
+    }
+    void enqueue(int x){
+        Node *temp=new Node(x);
+        if(front==NULL){
+            front=rear=temp;
+            return;
+        }
+        rear->next=temp;
+        rear=temp;
+        size++;
+    }
+    void dequeue(){
+        if(front==NULL){return;}
+        Node *temp=front;
+        front=front->next;
+        if(front==NULL){rear=NULL;}
+        delete(temp);
+        size--;
+    }
+    int getSize(){return size;}
+};
